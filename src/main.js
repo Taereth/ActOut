6dbfab588097ffe360da6d159543e8946894d1f9
Taereth <orell.endres@stud.hslu.ch>
2020-04-08 16:07:56 +0200

    First commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import Ionic from "@ionic/vue"
+import "@ionic/core/css/core.css"
+import "@ionic/core/css/ionic.bundle.css"
+
+
+Vue.config.productionTip = false;
+Vue.use(Ionic);
+
+
+// router
+import { IonicVueRouter } from "@ionic/vue";
+Vue.use(IonicVueRouter);
+
+const router = new IonicVueRouter({
+  routes: [
+    { path: "/", redirect: "/home" },
+    {
+      path: "/home",
+      name: "home",
+      component: () =>
+        import(/* webpackChunkName: "home" */ "@/components/HomePage"),
+    },
+    {
+      path: "/new-item",
+      name: "new-item",
+      component: () =>
+        import(/* webpackChunkName: "new-item" */ "@/components/NewItemPage"),
+}
+  ]
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
